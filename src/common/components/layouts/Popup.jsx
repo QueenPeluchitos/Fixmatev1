f9e371a358510ede4e63ac441c7cb7263a567d13
Peluchitos <un.peluchito@gmail.com>
2025-05-09 13:39:38 -0600

    Avances de ruteo

--- src/common/components/layouts/Popup.jsx
+++ src/common/components/layouts/Popup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AlertTriangle } from "lucide-react";
+import { Link } from "react-router-dom";  // Importa el Link desde react-router-dom
 
 const Popup = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -28,9 +29,11 @@ const Popup = () => {
             <p className="text-md font-medium mb-3 leading-snug">
               ¿Quieres formar parte de nuestro equipo?
             </p>
-            <button className="bg-yellow-400 text-indigo-900 font-semibold py-2 px-4 rounded-full shadow hover:bg-yellow-500 transition duration-300">
-              ¡Regístrate ahora!
-            </button>
+            <Link to="/registro-profesionista">
+              <button className="bg-yellow-400 text-indigo-900 font-semibold py-2 px-4 rounded-full shadow hover:bg-yellow-500 transition duration-300">
+                ¡Regístrate ahora!
+              </button>
+            </Link>
           </div>
         )}
       </div>
--- src/features/PerfilUsuario/PerfilUsuario.jsx
+++ src/features/PerfilUsuario/PerfilUsuario.jsx
@@ -55,7 +55,7 @@ export default function PerfilUsuario() {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-4">
+    <div className="max-w-6xl mx-auto p-4 my-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Columna izquierda - Perfil de usuario */}
         <div className="flex flex-col items-center">
